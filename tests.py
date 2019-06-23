2fc0558e93ca2c49c0a7470334bfddf4b5e7f4f0
TwistedLlama <rocket.chipco@gmail.com>
2019-06-23 11:24:45 +0200

    Add .gitignore Add manage.py, the tool for manage all the project

---
+++ tests.py
@@ -0,0 +1,8 @@
+import Tile
+import unittest
+
+class TestSpriteEditorMethods(unittest.TestCase):
+	def test_split(self):
+		self.assertEqual(test.new(), 1)
+		with self.assertRaises(TypeError):
+			s.split(2)
