f2db84f6cb1a32c125d1dfbe7dda424aaf8c414b
TwistedLlama <rocket.chipco@gmail.com>
2019-07-18 14:33:18 +0200

    Finished Palette Editor Gtk version

--- Utils/Palette Editor/Gtk version/Palette Editor.py
+++ Utils/Palette Editor/Gtk version/Palette Editor.py
@@ -1,170 +1,142 @@
 import os
-import pygame
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf, Gdk
 
-class MButton:
-    def __init__(self, x, y, w, h, tag, function):
-        self.coords = (x, y, w, h)
-        self.function = function
-        self.tag = tag
-
-    def update(self, mousePos):
-        if mousePos[0] > self.coords[0] and mousePos[0] < self.coords[0] + self.coords[2]:
-            if mousePos[1] > self.coords[1] and mousePos[1] < self.coords[1] + self.coords[3]:
-                return self.function()
-        return 0
-
-    def draw(self, display, font, color=(255, 255, 255)):
-        pygame.draw.rect(display, color,
-                         (self.coords[0] - 2, self.coords[1] - 2, self.coords[2] + 4, self.coords[3] + 4))
-        pygame.draw.rect(display, (255-color[0], 255-color[1], 255-color[2]), self.coords)
-        text = font.render(self.tag, False, color)
-        text = pygame.transform.scale(text, (self.coords[2] - 2, self.coords[3] - 2))
-        textrect = text.get_rect()
-        textrect.topleft = (self.coords[0] + 2, self.coords[1] + 2)
-        display.blit(text, textrect)
-
-
-class Main:
-    def __init__(self, palette):
-        pygame.init()
-        self.display = pygame.display.set_mode((640, 480))
-        self.minimap = pygame.Surface((128, 128))
-        self.editor = pygame.Surface((8, 8))
-        pygame.display.set_caption("Palette Editor")
-        self.clock = pygame.time.Clock()
-        self.data = self.new()
-        self.mouse = [0, 0]
-        self.currentsprite = [0, 0]
-        self.cursor = 0
-        self.palette = palette
-        self.font = pygame.font.SysFont("", 20, bold=True)
-        self.RUNNING = True
-        self.btn_new = MButton(2, 2, 212, 100, "New", self.new)
-        self.btn_open = MButton(214, 2, 212, 100, "Open", self.open)
-        self.btn_save = MButton(428, 2, 212, 100, "Save", self.save)
-        self.btn_colors = []
-        for i in range(0, 8):
-            self.btn_colors.append(MButton(2+(636/8)*i, 150, int(636/8), 290, str(i+1), self.color))
-        self.button = [0, 0, 0]
-        self.pos = [0, 0]
-        self.handler()
-
-    def color(self):
-        root = Tk()
-        color = tkcp.askcolor(color="red", parent=None, title=("Choose Color"), alpha=False)
-        root.destroy()
-        if not color[0] == None:
-            return color[0]
-        return 0
-
-    def save(self):
-        root = Tk()
-        file = filedialog.asksaveasfile(initialdir=os.getcwd(), title='Choose a Folder to save file into',
-                                        defaultextension=".pal")
-        root.destroy()
-        try:
-            for y in range(0, len(self.data)):
-                for x in range(0, len(self.data[y])):
-                    data = str(self.data[y][x])
-                    if len(data) == 1:
-                        data = "00"+data
-                    elif len(data) == 2:
-                        data = "0"+data
-                    file.write(data)
-            file.write(
-                "this data and the programs associated with it are made by Rocket-chip team, DON'T STEAL THEM, and credit us")
-        except:
-            pass
-
-    def new(self):
-        data = []
-        for y in range(0, 8):
-            data.append((255, 255, 255))
-        return data
-
-    def open(self):
-        root = Tk()
-        file = filedialog.askopenfile(initialdir=os.getcwd(), title='Choose a Palette file',
-                                      filetypes=(("PAL files", "*.pal"), ("All files", "*.*")))
-        root.destroy()
-        try:
-            data = file.read()
-            file.close()
-        except:
-            pass
-        else:
-            temp = []
-            for y in range(0, 8):
-                temp.append((int(data[y*9+0:y*9+0+3]),int(data[y*9+3:y*9+3+3]), int(data[y*9+6:y*9+6+3])))
-            return temp
-
-    def handler(self):
-        while self.RUNNING:
-            self.events()
-            self.update()
-            self.draw()
-            self.clock.tick(60)
-
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.RUNNING = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.RUNNING = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    if event.pos[1] < 100:
-                        openr = self.btn_open.update(event.pos)
-                        if openr:
-                            self.data = openr
-                        newr = self.btn_new.update(event.pos)
-                        if newr:
-                            self.data = newr
-                        palr = self.btn_save.update(event.pos)
-                        if palr:
-                            self.palette = palr
-                    for i in range(0, 8):
-                        color = self.btn_colors[i].update(event.pos)
-                        if color:
-                            self.data[i] = color
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    self.button = [0, 0, 0]
-
-    def update(self):
-        self.pos = pygame.mouse.get_pos()
-
-    def draw(self):
-        self.display.fill(self.palette[0])
-        # draw buttons
-        self.btn_new.draw(self.display, self.font)
-        self.btn_open.draw(self.display, self.font)
-        self.btn_save.draw(self.display, self.font)
-        for i in range(0, 8):
-            self.btn_colors[i].draw(self.display, self.font, color=self.data[i])
-
-        # update dislpay
-        pygame.display.update()
-
-
 class Handler:
+    def __init__(self):
+        self.data = [(255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255)]
+        self.file_path = None
+
     def onDestroy(self, *args):
         Gtk.main_quit()
 
-    def test(self, button):
-            print("\"Open\" button was clicked")
+    def new_file(self, button):
+        color = Gdk.RGBA()
+        color.red = 0.0
+        color.green = 0.0
+        color.blue = 0.0
+        color.alpha = 1.0
+        for i in range(1,9):
+            button = builder.get_object("button"+str(i))
+            button.set_rgba(color)
+        self.file_path = None
+        self.data = []
+        for y in range(1, 9):
+            self.data.append((255, 255, 255))
+
+    def open_file(self, path):
+        with open(path, "r") as file:
+            data = file.read()
+            print(data)
+            temp = []
+            for y in range(0, 8):
+                temp.append((int(data[y*9+0:y*9+0+3]),int(data[y*9+3:y*9+3+3]), int(data[y*9+6:y*9+6+3])))
+            print(temp)
+            self.data = temp
+            #reinitialize colors of colrbuttons
+            for i in range(1,9):
+                print("i :",i)
+                color = Gdk.RGBA()
+                color.alpha = 1.0
+                print(self.data[i-1])
+                color.parse("rgb("+str(self.data[i-1][0])+","+str(self.data[i-1][1])+","+str(self.data[i-1][2])+")")
+                button = builder.get_object("button"+str(i))
+                button.set_rgba(color)
+
+
+
+    def on_file_clicked(self, button):
+        dialog = Gtk.FileChooserDialog("Please choose a file", builder.get_object("main_window"),
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self.file_path = dialog.get_filename()
+            self.open_file(dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name(".pal files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_py = Gtk.FileFilter()
+        filter_py.set_name("Text files")
+        filter_py.add_mime_type("text/x-python")
+        dialog.add_filter(filter_py)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
+    def save(self, button):
+        print(self.data)
+        print(self.file_path)
+        if self.file_path != None:
+            with open(self.file_path, "w") as file:
+                try:
+                    for y in range(0, len(self.data)):
+                        for x in range(0, len(self.data[y])):
+                            data = str(self.data[y][x])
+                            if len(data) == 1:
+                                data = "00"+data
+                            elif len(data) == 2:
+                                data = "0"+data
+                            file.write(data)
+                    file.write("this data and the programs associated with it are made by Rocket-chip team, DON'T STEAL THEM, and credit us")
+                except:
+                    print("Probleme")
+        else:
+            no_file_dialog.run()
+            no_file_dialog.hide()
+
+    def on_save_as_clicked(self, button):
+        file_dialog.run()
+        file_dialog.hide()
+
+    def on_dialog_save_clicked(self,button):
+        if builder.get_object("file_name_input").get_text() != "":
+            self.file_path = file_dialog.get_current_folder()+"/"+builder.get_object("file_name_input").get_text()+".pal"
+            self.save(None)
+            file_dialog.hide()
+        else:
+            no_file_dialog.run()
+            no_file_dialog.hide()
+
+    def on_color_set(self, button):
+        data_tuple = button.get_rgba().to_string().split("(")
+        data_tuple = data_tuple[1].split(")")
+        data_tuple = data_tuple[0].split(",")
+        r = int(data_tuple[0])
+        g = int(data_tuple[1])
+        b = int(data_tuple[2])
+        final_tuple = (r,g,b)
+        self.data[int(button.get_name()[len(button.get_name())-1])-1] = final_tuple
 
     def on_gtk_about_activate(self, menuitem, data=None):
+        print(self.file_path)
+        print(self.data)
         print("help about selected")
         response = aboutdialog.run()
         aboutdialog.hide()
 
-if __name__ == "__main__":
+    def close_no_file_dialog(self, button):
+        no_file_dialog.hide()
 
+
+if __name__ == "__main__":
 
     #add ui from an xml file
     builder = Gtk.Builder()
@@ -175,25 +147,15 @@ if __name__ == "__main__":
     window = builder.get_object("main_window")
     window.set_default_size(750, 200)
 
-    #add about dialo
+    #add about dialog
     aboutdialog = builder.get_object("about_dialog")
-    
+
+    #add file chooser
+    file_dialog = builder.get_object("file_chooser_dialog")
+    no_file_dialog = builder.get_object("no_file_dialog")
+
     #init window
     window.show_all()
 
     #main loop
     Gtk.main()
-
-
-
-
-
-
-    # from tkinter import *
-    # from tkinter import filedialog
-    # import tkcolorpicker as tkcp
-
-    # Main(((0, 0, 0), (255, 255, 255), (173, 173, 173), (81, 81, 81), (0, 0, 255), (255, 255, 0), (0, 255, 255),
-    #       (255, 0, 255)))
-    # pygame.quit()
-# this program was made ENTIERLY by Geek_Joystick, DON'T STEAL IT, and credit me.
